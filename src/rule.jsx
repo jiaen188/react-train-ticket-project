f6d084b979f967ca60b22c6541ea3d0c3b6067cd
gulifan <gulifan@dxy.cn>
2020-05-06 19:36:06 +0800

    feat: Hooks的使用法则

---
+++ src/rule.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+// Hook 使用规则
+// 只能在函数最外层调用 Hook。不要在循环、条件判断或者子函数中调用。
+// 只能在 React 的函数组件中调用 Hook。不要在其他 JavaScript 函数中调用。
+
+function App() {
+
+}
+
+function useLogin() {
+  const [login, setLogin] = useState()
+
+  useEffect(() => {
+
+  })
+}
+
+function fectNews() {
+  const [pageNo, setPageNo] = useState()
+}
