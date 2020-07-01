b70b351c0d1c61562f7ad0d6dfece98ca5837b87
gulifan <gulifan@dxy.cn>
2020-07-01 11:00:14 +0800

    feat: 城市选择浮层-搜索建议

--- src/common/CitySelector.jsx
+++ src/common/CitySelector.jsx
@@ -20,7 +20,7 @@ const CityItem = memo(function CityItem(props) {
   )
 })
 
-CityItem.prototype = {
+CityItem.Prototypes = {
   name: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
 }
@@ -49,7 +49,7 @@ const CitySection = memo(function CitySection(props) {
   )
 })
 
-CitySection.prototype = {
+CitySection.Prototypes = {
   title: PropTypes.string.isRequired,
   cities: PropTypes.array,
   onSelect: PropTypes.func.isRequired
@@ -110,10 +110,78 @@ const CityList = memo(function CityList(props) {
   )
 })
 
-CityList.prototype = {
+CityList.Prototypes = {
   sections: PropTypes.array.isRequired,
   onSelect: PropTypes.func.isRequired,
   toAlpha: PropTypes.func.isRequired
+}
+
+const SuggestItem = memo(function SuggestItem(props) {
+  const {
+    name,
+    onClick
+  } = props
+
+  return (
+    <li className="city-suggest-li" onClick={() => onClick(name)}>{ name }</li>
+  )
+})
+
+SuggestItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+const Suggest = memo(function Suggest(props) {
+  const {
+    searchKey,
+    onSelect
+  } = props
+
+  const [result, setResult] = useState([])
+
+  useEffect(() => {
+    fetch('/rest/search?key=' + encodeURI(searchKey))
+      .then(res => res.json())
+      .then(data => {
+        const {
+          result,
+          searchKey: sKey
+        } = data
+        if (sKey === searchKey) {
+          setResult(result)
+        }
+      })
+  }, [searchKey])
+
+  const fallBackResult = useMemo(() => {
+    if (!result.length) {
+      return [{ display: searchKey }]
+    }
+    return result
+  }, [result, searchKey])
+
+  return (
+    <div className="city-suggest">
+      <ul className="city-suggest-ul">
+        {
+          fallBackResult.map(item => {
+            return (
+              <SuggestItem 
+                key={item.display}
+                name={item.display}
+                onClick={onSelect}></SuggestItem>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+})
+
+Suggest.propTypes = {
+  searchKey: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
 }
 
 const CitySelector = memo(function CitySelector (props) {
@@ -183,6 +251,11 @@ const CitySelector = memo(function CitySelector (props) {
             hidden: key.length === 0
           })}>&#xf063;</i>
       </div>
+      {
+        Boolean(key) && (
+          <Suggest searchKey={key} onSelect={key => onSelect(key)}></Suggest>
+        )
+      }
       { outputCitySections() }
     </div>
   )
