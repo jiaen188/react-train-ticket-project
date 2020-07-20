ff16df2704a61830feb7d6ae02b32cf70b26a61b
gulifan <gulifan@dxy.cn>
2020-07-20 22:23:49 +0800

    feat: 数据结构与模块设计

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,11 +1,27 @@
 import { connect } from 'react-redux'
+import React from 'react'
+
+import Nav from '../common/Nav'
+import List from './List'
+import Bottom from './Bottom'
+
 import './App.css'
 
 function App(props) {
-
+  return (
+    <div>
+      <Nav></Nav>
+      <List></List>
+      <Bottom></Bottom>
+    </div>
+  )
 }
 
 export default connect(
-  function mapStateToProps(state) {},
-  function mapDispatchToProps(dispatch) {}
+  function mapStateToProps(state) {
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch }
+  }
 )(App)
